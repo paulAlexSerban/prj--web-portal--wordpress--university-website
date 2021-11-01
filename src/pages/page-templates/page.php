831faad6050283b81f62f957251aabb3daee9030
paulAlexSerban <paul.alex.serban@gmail.com>
2021-11-01 14:22:47 +0200

    uw_36 | implemented conditional metabox/breadcrumb on page

--- src/pages/page-templates/page.php
+++ src/pages/page-templates/page.php
@@ -1,12 +1,22 @@
-<?php
+<?php 
+// functions
+/**
+ * getParent() returns 0 if it is called for on a parent page or the id of the parent page
+ */
+function getParent() {
+  $theParent = wp_get_post_parent_id(get_the_id());
+  return $theParent;
+}
+?>
 
-  get_header();
+<?php  get_header(); ?>
 
+<?php 
   while(have_posts()) {
     the_post(); ?>
     
   <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('../../uploads/images/ocean.jpg') ?>);"></div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('../../uploads/2021/11/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <div class="page-banner__intro">
@@ -17,9 +27,18 @@
 
   <div class="container container--narrow page-section">
 
+  <?php
+  if(getParent()) { ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+      <p class="metabox__content">
+        <a class="metabox__parent-link" href="<?php echo get_permalink(getParent()); ?>">
+          <i class="fa fa-home" aria-hidden="true"></i>
+           Back to <?php echo get_the_title(getParent()); ?>
+        </a class="metabox__current-page"> 
+          <span class="metabox__main"><?php the_title() ?></span>
+      </p>
     </div>
+  <?php } ?>
     
     <!--     
     <div class="page-links">
