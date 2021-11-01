52d41ae987d66f38920f40b0f0833ea49a9ec71a
paulAlexSerban <paul.alex.serban@gmail.com>
2021-11-01 13:08:58 +0200

    uw_35 | updated positioning rules using mixis

--- src/pages/page-templates/page.php
+++ src/pages/page-templates/page.php
@@ -5,12 +5,12 @@
   while(have_posts()) {
     the_post(); ?>
     
-    <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('../../uploads/images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <div class="page-banner__intro">
-        <p>DONT FORGET TO REPLACE ME LATER</p>
+        <p>DON'T FORGET TO REPLACE ME LATER</p>
       </div>
     </div>  
   </div>
@@ -21,7 +21,7 @@
       <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
     </div>
     
-<!--     
+    <!--     
     <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
@@ -37,8 +37,6 @@
 
   </div>
     
-  <?php }
+  <?php } ?>
 
-  get_footer();
-
-?>
+  <?php get_footer(); ?>
