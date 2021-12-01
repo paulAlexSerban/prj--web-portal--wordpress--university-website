63e68020749e6e3ab944b74b110b769dc8f2fec8
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-02 00:12:36 +0200

    uw_79 | added professor post type

--- src/plugins/mu-plugins/university-post-types.php
+++ src/plugins/mu-plugins/university-post-types.php
@@ -3,6 +3,7 @@
  * Creates custom post types
  */
 function university_post_types() {
+  // Events post types
   register_post_type('event', array(
     'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array(
@@ -19,6 +20,23 @@ function university_post_types() {
       'singular_name' => 'Event'
     ), 
     'menu_icon' => 'dashicons-calendar'
+  ));
+
+  // Programmes post types
+
+  // Professors post types
+  register_post_type('professor', array(
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Professors',
+      'add_new_item' => 'Add New Professor',
+      'edit_item' => 'Edit Professor',
+      'all_items' => 'All Professors',
+      'singular_name' => 'Professors'
+    ), 
+    'menu_icon' => 'dashicons-welcome-learn-more'
   ));
 }
 
