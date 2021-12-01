a22523b831daefa1905a9abbceca2b6926c6832a
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-01 20:45:35 +0200

    uw_69 | updated archive.php with the new the_archive_title

--- src/ux-ui/components/pages/archive/archive.php
+++ src/ux-ui/components/pages/archive/archive.php
@@ -4,15 +4,8 @@
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('../../uploads/2021/11/ocean.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title">
-      <?php 
-      if(is_category()) {
-        single_cat_title();
-      } else if(is_author()) {
-        echo 'Posts by: '; the_author();
-      } else {
-        the_archive_title();
-      }
-    ?></h1>
+      <?php the_archive_title(); ?>
+    </h1>
     <div class="page-banner__intro">
       <p><?php the_archive_description();?></p>
     </div>
