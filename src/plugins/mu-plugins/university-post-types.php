07bfec1bed5c25a5dacf3c42c89fb39a1c5be6b5
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-01 19:00:47 +0200

    uw_63 | implemented basic 'event' custom post type

--- src/plugins/mu-plugins/university-post-types.php
+++ src/plugins/mu-plugins/university-post-types.php
@@ -5,8 +5,13 @@
 function university_post_types() {
   register_post_type('event', array(
     'public' => true,
+    'show_in_rest' => true,
     'labels' => array(
-      'name' => 'Events'
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
     ), 
     'menu_icon' => 'dashicons-calendar'
   ));
