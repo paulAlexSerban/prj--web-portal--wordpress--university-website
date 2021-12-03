b09f3ebe09a1efd8799344edfb9d4abf4d9a34b7
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-03 16:47:00 +0200

    uw_83 | added the logic to sort events

--- src/ux-ui/components/pages/front-page/front-page.php
+++ src/ux-ui/components/pages/front-page/front-page.php
@@ -17,8 +17,11 @@
 
         <?php 
           $homepageEvents = new WP_Query(array(
-            'posts_per_page' => 2,
-            'post_type' => 'event'
+            'posts_per_page' => 5,
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC'
           ));
 
           while($homepageEvents->have_posts()) {
