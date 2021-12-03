b6fb87f29d73561e157f368e55d289a3ebe81fd1
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-03 19:37:39 +0200

    uw_93 | added custom query for program archive

--- src/required/functions.php
+++ src/required/functions.php
@@ -39,19 +39,25 @@ function add_additional_class_on_li($classes, $item, $args) {
 add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1, 3);
 
 function university_adjust_queries($query) {
-  if(!is_admin() && is_post_type_archive('event') && $query->is_main_query()) {
-    $today = date('Ymd');
-    $query->set('meta_key', 'event_date');
-    $query->set('orderby', 'meta_value_num');
-    $query->set('order', 'ASC');
-    $query->set('meta_query', array(
-      array(
-        'key' => 'event_date',
-        'compare' => '>=',
-        'value' => $today,
-        'type' => 'numeric'
-      ),
-    ));
+  if(!is_admin() && $query->is_main_query() ) {
+    if(is_post_type_archive('event') ) {
+      $today = date('Ymd');
+      $query->set('meta_key', 'event_date');
+      $query->set('orderby', 'meta_value_num');
+      $query->set('order', 'ASC');
+      $query->set('meta_query', array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today,
+          'type' => 'numeric'
+        ),
+      ));
+    } else if(is_post_type_archive('program')) {
+      $query->set('orderby', 'title');
+      $query->set('order', 'ASC');
+      $query->set('posts_per_page', -1);
+    }
   }
 }
 
