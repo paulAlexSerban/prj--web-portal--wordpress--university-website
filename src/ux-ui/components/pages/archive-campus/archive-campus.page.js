993f4d353a8215ada10f17b4954c9ff58b150fb1
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-04 07:04:36 +0200

    uw_109 | created archive for capus posts

---
+++ src/ux-ui/components/pages/archive-campus/archive-campus.page.js
@@ -0,0 +1,1 @@
+import './archive-campus.page.scss';
---
+++ src/ux-ui/components/pages/archive-campus/archive-campus.php
@@ -0,0 +1,25 @@
+<?php get_header();
+      pageBanner(array(
+        'title' => 'Our Campuses',
+        'subtitle' => 'We have several conveniently located campuses'
+      )); ?>
+
+<div class="container container--narrow page-section">
+
+  <ul class="link-list min-list">
+  <?php 
+  while(have_posts()) {
+    the_post(); ?>
+      <li>
+        <a href="<?php the_permalink();?>">
+          <?php the_title();?>
+        </a>
+      </li>
+  <?php } ?>
+  </ul>
+
+  <?php echo paginate_links(); ?>
+
+</div>
+
+<?php get_footer(); ?>
