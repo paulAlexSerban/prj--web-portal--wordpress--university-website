8b6f82d9ba84d4926f8024ae8b65c28be54f3ff1
paulAlexSerban <paul.alex.serban@gmail.com>
2021-12-04 03:52:24 +0200

    uw_99 | added nre thumbnail sizes

--- src/ux-ui/components/pages/single-program/single-program.php
+++ src/ux-ui/components/pages/single-program/single-program.php
@@ -56,13 +56,16 @@ while(have_posts()) {
         
     if($relatedProfessors->have_posts()) { ?>
       <hr class="section-break">
-      <h2 class="headline headline--medium">Related <?php echo get_the_title()?> Events</h2>
-      <ul class="link-list min-list">
+      <h2 class="headline headline--medium"><?php echo get_the_title()?> Professors</h2>
+      <ul class="professor-cards">
         <?php
         while($relatedProfessors->have_posts()) {
           $relatedProfessors->the_post(); ?>
-          <li>
-            <a href="<?php the_permalink();?>"><?php the_title();?></a>
+          <li class="professor-card__list-item">
+            <a class="professor-card" href="<?php the_permalink();?>">
+              <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape');?>" alt="">
+              <span class="professor-card__name"><?php the_title();?></span>
+            </a>
           </li>
           <?php 
         }
